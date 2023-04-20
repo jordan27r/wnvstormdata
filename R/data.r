5ea26299a2ab6d2c8eb586229dd28dd18cbfb5f8
Jordan Robinson <jordan227r@gmail.com>
2023-04-20 14:21:01 -0600

    created README

--- R/data.r
+++ R/data.r
@@ -4,21 +4,17 @@
 #'  from 2002-2021 filtered against NOAA hurricane data to determine if there
 #'  was a hurricane exposure for each observation.
 #'
-#' @title {Primary Dataset in Package}
-#' @description {WNV w/ storms}
-#' @name {wnv_2002_2021_stormexposure}
 #'
 #' @format ## `wnv_2002_2021_stormexposure`
 #' A data frame with 19,272 rows and 6 columns:
 #' \describe{
-#'   \item{Parish}{Parish name}
-#'   \item{Fips}{5 Number Fips Cpde for Parish}
-#'   \item{BeginningDate}{Date of Beginning of CDC Observance Period}
-#'   \item{EndDate}{Date of End of CDC Observance Period}
-#'   \item{Cases}{Number of Cases During Observance Week}
-#'   \item{Exposure}{If Parish Experienced a Storm with Winds of at Least 34 Knots}
-#'   \item{Storm}{If Exposed, what Storm Occured}
-#'   ...
+#'   \item{Parish}{Character string with Parish name}
+#'   \item{Fips}{Character string with 5 Number FIPs Cpde for Parish}
+#'   \item{BeginningDate}{Date Object with Beginning of CDC Epidemiological Week}
+#'   \item{EndDate}{Date Object with End of CDC Epidemiological Week}
+#'   \item{Cases}{Double object with Number of Cases During Epidemiological Week}
+#'   \item{Exposure}{Double Object of Parish Exposure to a Storm with Winds of at Least 34 Knots, Denoted 1 for Exposure}
+#'   \item{Storm}{Characyer Obeject of what Stomr Occured if Exposed}
 #' }
 #'
 #'@examples
---
+++ README.md
@@ -0,0 +1,30 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# wnvstormdata
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The data provided in `wnvstormdata` integrates weekly data on tropical
+cyclone exposure and West Nile Virus incidence for parishes in Louisiana
+from 2002–2021 to determine if a storm occurred in each parish for the
+observed week. This data set provides researchers interested in the
+association between vector-borne disease prevalence and hurricanes with
+data on cases numbers and when hurricane exposure took place paired by
+location and time interval.
+
+## Installation
+
+You can install the development version of wnvstormdata from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("jordan27r/wnvstormdata", build_vignettes = TRUE)
+```
+
+## Example
+
+To get more information on `wnvstormdata`, you can open the Overview
+vignette with:
